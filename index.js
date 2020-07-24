aeea95dd6a2febf97a30d1df2411ecfd3ccdf4ed
Jacob <35826199+jacobWhite717@users.noreply.github.com>
2020-07-24 13:22:47 -0230

    Update index.js

--- index.js
+++ index.js
@@ -18,28 +18,6 @@ class Action {
         this.includeSymbols = JSON.parse(process.env.INPUT_INCLUDE_SYMBOLS || process.env.INCLUDE_SYMBOLS)
     }
 
-    _fromDir(startPath,filter){
-
-        //console.log('Starting from dir '+startPath+'/');
-
-        if (!fs.existsSync(startPath)){
-            console.log("no dir ",startPath);
-            return;
-        }
-
-        var files=fs.readdirSync(startPath);
-        for(var i=0;i<files.length;i++){
-            var filename=path.join(startPath,files[i]);
-            var stat = fs.lstatSync(filename);
-            if (stat.isDirectory()){
-                fromDir(filename,filter); //recurse
-            }
-            else if (filename.indexOf(filter)>=0) {
-                console.log('-- found: ',filename);
-            };
-        };
-    }
-
     _printErrorAndExit(msg) {
         console.log(`##[error]😭 ${msg}`)
         throw new Error(msg)
